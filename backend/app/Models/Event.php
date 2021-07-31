b2949b122a077a9bf43587690ad0ca17a4582282
Vishal24367 <vishal.yadav2279@gmail.com>
2021-08-01 03:20:19 +0700

    latest changes

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Schedule;
+use App\Model\User;
 
 class Event extends Model
 {
     use HasFactory;
+
+    public function schedules()
+    {
+        return $this->hasMany(Schedule::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ backend/database/migrations/2021_01_16_095022_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->date('schedule_date');
+            $table->softDeletesTz($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('schedules', function(Blueprint $table)
+        // {
+        //     $table->dropForeign('event_id');
+        // });
+        Schema::dropIfExists('schedules');
+    }
+}
